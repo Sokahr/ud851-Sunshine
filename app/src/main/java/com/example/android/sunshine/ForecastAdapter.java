216056eb274eb8e4affb37559babbc2601a26294
Benjamin Peter <ben61279@gmail.com>
2017-11-18 12:33:07 +0100

    done

---
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,96 @@
+package com.example.android.sunshine;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    public void setmWeatherData(String[] mWeatherData) {
+        this.mWeatherData = mWeatherData;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Called when RecyclerView needs a new {@link ForecastAdapterViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using {@link #onBindViewHolder(ForecastAdapterViewHolder, int)}
+     * . Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(ForecastAdapterViewHolder, int)
+     */
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.forecast_list_item, parent, false);
+
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ForecastAdapterViewHolder#itemView} to reflect the item at the given
+     * position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link ForecastAdapterViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Override {@link #onBindViewHolder(ForecastAdapterViewHolder, int)} instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        holder.mWeatherTextView.setText(mWeatherData[position]);
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        int count = 0;
+        if (mWeatherData != null) {
+            count = mWeatherData.length;
+        }
+
+        return count;
+    }
+
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        final TextView mWeatherTextView;
+
+        ForecastAdapterViewHolder(View itemView) {
+            super(itemView);
+            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+        }
+    }
+}
