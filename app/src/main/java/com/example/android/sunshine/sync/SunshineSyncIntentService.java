7d31ebce43f8d234171d1f3cc754e7aedd633dce
Benjamin Peter <ben61279@gmail.com>
2017-12-14 22:27:49 +0100

    DONE

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,37 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+
+// DONE (5) Create a new class called SunshineSyncIntentService that extends IntentService
+//  DONE (6) Create a constructor that calls super and passes the name of this class
+//  DONE (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+public class SunshineSyncIntentService extends IntentService {
+
+    public SunshineSyncIntentService() {
+        super(SunshineSyncIntentService.class.getName());
+    }
+
+    /**
+     * This method is invoked on the worker thread with a request to process.
+     * Only one Intent is processed at a time, but the processing happens on a
+     * worker thread that runs independently from other application logic.
+     * So, if this code takes a long time, it will hold up other requests to
+     * the same IntentService, but it will not hold up anything else.
+     * When all requests have been handled, the IntentService stops itself,
+     * so you should not call {@link #stopSelf}.
+     *
+     * @param intent The value passed to {@link
+     *               Context#startService(Intent)}.
+     *               This may be null if the service is being restarted after
+     *               its process has gone away; see
+     *               {@link Service#onStartCommand}
+     *               for details.
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+    }
+}
--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,38 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+//  Done (1) Create a class called SunshineSyncTask
+//  Done (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+//      DONE (3) Within syncWeather, fetch new weather data
+//      DONE (4) If we have valid results, delete the old data and insert the new
+public class SunshineSyncTask {
+    synchronized public static void syncWeather(Context context) {
+
+        try {
+            URL wheatherUrl = NetworkUtils.getUrl(context);
+            String responseFromHttpUrl = NetworkUtils.getResponseFromHttpUrl(wheatherUrl);
+            ContentValues[] contentValues = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, responseFromHttpUrl);
+
+            if (contentValues != null && contentValues.length != 0) {
+                ContentResolver contentResolver = context.getContentResolver();
+                contentResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI,
+                        null, null);
+                contentResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, contentValues);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
